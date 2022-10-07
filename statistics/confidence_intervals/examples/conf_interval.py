26614ce680b1227db2abe1c39ef195e16e00b715
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-10-07 15:34:19 +0300

    Finished Simple CI generation

--- statistics/confidence_intervals/examples/conf_interval.py
+++ statistics/confidence_intervals/examples/conf_interval.py
@@ -9,21 +9,35 @@
 
 import numpy as np 
 from scipy.stats import t
+import math
 
 # %%
+
+
+
+# %% [markdown]
+# z_val now is how many standard deviations we consider the potential sample means 
+# could go from the global mean
+# # %% 
+
+#%%
 
 x = np.random.normal(size=100)
 m = x.mean()
 s = x.std()
-dof = len(x)-1
-confidence = 0.95 
+sample_size = len(x)-1
+confidence = 0.99 
 alpha = 1-confidence
+number_of_stds = np.abs(t.ppf((alpha)/2,sample_size))
+print(number_of_stds)
 
-# %% [markdown]
-# t_crit now is how many standard deviations we consider the potential sample means 
-# could go from the global mean
-# # %% 
-t_crit = np.abs(t.ppf((alpha)/2,dof))
-print(t_crit)
+print(x)
+min_b = m - number_of_stds * (s/math.sqrt(sample_size))
+max_b = m + number_of_stds * (s/math.sqrt(sample_size))
+
+print(f"Confidence interval is {min_b}..{m}..{max_b}" )
+print(f"Width is {max_b-min_b}" )
+
+
 
 # %%
