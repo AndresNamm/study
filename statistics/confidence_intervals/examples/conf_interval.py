67e300cf36ddd352c8d241b5696983f2bb919cf5
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-10-24 14:34:19 +0300

    Added numpy

--- statistics/confidence_intervals/examples/conf_interval.py
+++ statistics/confidence_intervals/examples/conf_interval.py
@@ -2,7 +2,10 @@
 # %% [markdown]
 # # Confidence Intervals
 # + [Quick Reference to my fantastic CI tutorial](https://github.com/AndresNamm/study/blob/main/statistics/confidence_intervals/CONFIDENCE%20INTERVALS.pdf)
-
+# + [This code is based on this blog post](https://towardsdatascience.com/how-to-calculate-confidence-intervals-in-python-a8625a48e62b)    
+#
+# As you can see, we are taking the data from Uniform distribution. Due to the Central Limit Theorem (CLT) we can assume that its mean if taken as samples of size N will follow a normal distribution
+# with Std of $\frac{\sigma}{\sqrt{n}}$
 # %%
 import numpy as np 
 from scipy.stats import t,norm
@@ -20,14 +23,16 @@ confidence = 0.99
 alpha = 1-confidence
 
 # %% [markdown]
-# z_val Where in the standard normal distribution a value outside the confidence interval would be located
+# z_val - Where in the standard normal distribution a value outside the confidence interval would be located. As standard deviation within the standardized normal distribution
+# is 1 then z_val tells how many standard deviations in a **standard** normal distribution the value should go away from the mean to not fall inside the range of confidence inteval.
 #%%
 z_val = np.abs(norm.ppf((alpha)/2))
 sample_size=len(x)
 confidence = 0.99 
 alpha = 1-confidence
-min_b = m - z_val * (s/math.sqrt(sample_size))
-max_b = m + z_val * (s/math.sqrt(sample_size))
+ci_std = (s/math.sqrt(sample_size)) # FROM THE CLT
+min_b = m - z_val * ci_std
+max_b = m + z_val * ci_std
 
 def print_statistics(m:float,s:float,sample_size:int,confidence:float,min_b:float,max_b:float):
     print(f"Median: {m}")
@@ -41,8 +46,8 @@ def print_statistics(m:float,s:float,sample_size:int,confidence:float,min_b:floa
 print_statistics(m=m,s=s,sample_size=sample_size,confidence=confidence,min_b=min_b,max_b=max_b)
 # %% [markdown]
 # # T distribution
-# + [First Tutorial I found](https://towardsdatascience.com/how-to-calculate-confidence-intervals-in-python-a8625a48e62b)
-# An important note is that in this case we are using a [t distribution](https://www.investopedia.com/terms/t/tdistribution.asp) instead of a normal distribution. 
+#     
+# We are doing the same thing here as in previous lines but an important note is that in this case we are using a [t distribution](https://www.investopedia.com/terms/t/tdistribution.asp) instead of a normal distribution. 
 # Depending on the degrees of freedom (sample size-1). The t-distribution is useful when you have small sample sizes.
 # It has heavier tails so it's more likely give out extreme values. This means
 # 1. The t-distribution based Confidence Interval is wider. (Less accurate)         
@@ -54,11 +59,9 @@ print_statistics(m=m,s=s,sample_size=sample_size,confidence=confidence,min_b=min
 # 2. If the sample size is more than 30 the t distribution closely resembles the normal distribution so you can pretty much use both.  
 # Based on this we could have used both the t and the z distribution here. 
 #        
-#%%
-
 # %% [markdown]
 # Only things you have to do diffetently when calculating t-distribution
-# + degrees of freedom = sample_size -1
+# + Add degrees of freedom variable = sample_size -1
 # + t_val = Where in the standard t-distribution a value outside the confidence interval would be located
 # %%
 degrees_of_freedom = len(x)-1
@@ -66,14 +69,32 @@ t_val = np.abs(t.ppf((alpha)/2,degrees_of_freedom))
 
 # %% [markdown]
 # sample_size now determines the confidence interval width
-# %% 
-t_min_b = m - t_val * (s/math.sqrt(sample_size))
-t_max_b = m + t_val * (s/math.sqrt(sample_size))
+# %%
+ci_std = (s/math.sqrt(sample_size)) # FROM THE CLT 
+t_min_b = m - t_val * ci_std
+t_max_b = m + t_val * ci_std
 
 
 print_statistics(m=m,s=s,sample_size=sample_size,confidence=confidence,min_b=t_min_b,max_b=t_max_b)
 
- 
+#%% [markdown] 
+# I will now show an example how Uniform distribution means will follow a normal distribution. 
+## Uniform distribution from 100*100 Sample
+nr_samples = 100
+sample_size = 100
+full_dataset = np.empty([1,10])
+print(full_dataset)
+#%%
+
+for i in range(nr_samples):
+    x = np.random.uniform(size=sample_size) 
+    x_positive = x-min(x)
+    x_scaled = x_positive/max(x_positive)*100
+    x = x_scaled
+
+
+
+
 
 #%% [markdown]
 # Comparing t-distribution width with normal distribution width when sample size is 100
@@ -88,7 +109,15 @@ print(f"T-distribution max boundary {t_max_b}")
 print(f"Normal dist width {max_b-min_b}")
 print(f"t dist width {t_max_b-t_min_b}")
 
+
+
+
 #%% 
+
+
+
+
+
 counts, bins = np.histogram(x_scaled)
 plt.hist(bins[:-1],len(bins)-1,weights=counts)
 plt.axvline(m, color='k', linestyle='dashed', linewidth=1)
@@ -106,6 +135,13 @@ len(counts)
 # %%
 # %% [markdown]
 # Lets draw 100 times from uniform distribution
+#%%
+
+x = np.random.uniform(size=100) 
+x_positive = x-min(x)
+x_scaled = x_positive/max(x_positive)*100
+x = x_scaled
+
 # %%
 all_samples = []
 for i in range(100):
