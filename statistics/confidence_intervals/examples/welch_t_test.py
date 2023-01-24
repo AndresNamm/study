ee4e5e5bcac8a63915c55631b058a5a186de40c4
Andres Namm <andresnamm@microsoft.com>
2023-01-24 08:09:28 +0200

    Welch test

---
+++ statistics/confidence_intervals/examples/welch_t_test.py
@@ -0,0 +1,14 @@
+#%% [markdown]
+# # Welch's t-test
+# In this example I will calculate the p value based on welch test and perform hypothesis testing on the data that is randomly generated. Then I will visualize the results.
+
+
+#%%
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+
+
+# %%
